1e408589455887a6aa19c4b50beba66e1acf907c
FireSouls25 <david.castillofl@campusucc.edu.co>
2026-03-22 17:33:42 -0500

    Fix de sha

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,11 +27,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../../upload-keystore.jks")
+            storePassword = "sign123456"
+            keyAlias = "upload"
+            keyPassword = "sign123456"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
