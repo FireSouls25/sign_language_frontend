ca2f16165923d12156751449153dc96408c021b0
FireSouls25 <david.castillofl@campusucc.edu.co>
2026-03-22 20:33:20 -0500

    Abrir navegador externo para OAuth

--- lib/services/google_auth_service.dart
+++ lib/services/google_auth_service.dart
@@ -1,24 +1,9 @@
-import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../config/api_config.dart';
 
 class GoogleAuthService {
   Future<void> signIn() async {
     final url = Uri.parse('${ApiConfig.baseUrl}/api/auth/login/google');
-    await launchUrl(
-      url,
-      customTabsOptions: const CustomTabsOptions(
-        colorSchemes: CustomTabsColorSchemes(
-          colorScheme: CustomTabsColorScheme.system,
-        ),
-        showTitle: false,
-        urlBarHidingEnabled: true,
-        shareState: CustomTabsShareState.off,
-        instantAppsEnabled: false,
-      ),
-      safariVCOptions: const SafariViewControllerOptions(
-        barCollapsingEnabled: true,
-        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
-      ),
-    );
+    await launchUrl(url, mode: LaunchMode.externalApplication);
   }
 }
